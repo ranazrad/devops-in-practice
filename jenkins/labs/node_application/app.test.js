caca36141de8d2cad2e6227bd512fa751914d7a4
Ran Azrad <ranazrad@gmail.com>
2025-01-11 22:03:57 +0200

    Update app.test.js

--- jenkins/labs/node_application/app.test.js
+++ jenkins/labs/node_application/app.test.js
@@ -1,5 +1,21 @@
 const request = require('supertest');
-const app = require('./app');
+const app = require('./app'); // Import your app
+
+let server; // Store the server instance
+
+beforeAll(() => {
+    // Start the server before tests
+    server = app.listen(3000, () => {
+        console.log('Test server running at http://localhost:3000/');
+    });
+});
+
+afterAll(() => {
+    // Close the server after tests
+    server.close(() => {
+        console.log('Test server closed');
+    });
+});
 
 describe('GET /', () => {
     it('should return a message', async () => {
