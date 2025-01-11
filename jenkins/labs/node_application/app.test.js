9f07c83ceda0d1aa9819b5ad0d8ff59f8be29b69
Ran Azrad <ranazrad@gmail.com>
2025-01-11 21:28:03 +0200

    Create app.test.js

---
+++ jenkins/labs/node_application/app.test.js
@@ -0,0 +1,11 @@
+javascript
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /', () => {
+    it('should return a message', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('message', 'Hello, Jenkins!');
+    });
+});
