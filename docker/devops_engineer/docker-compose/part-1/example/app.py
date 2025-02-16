f5ad6dca1318d9ae906cdb6136df45e5b6e59e3d
Ran Azrad <ranazrad@gmail.com>
2025-02-16 08:04:47 +0200

    Update app.py
    update port

--- docker/devops_engineer/docker-compose/part-1/example/app.py
+++ docker/devops_engineer/docker-compose/part-1/example/app.py
@@ -37,4 +37,4 @@ def add_song():
     return jsonify({"title": title, "artist": artist})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5010)
