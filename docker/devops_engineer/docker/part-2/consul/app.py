5979794ef3bcc672da9cff38ce8a390c1b46fc86
Ran Azrad <ranazrad@gmail.com>
2025-02-16 08:11:32 +0200

    Update app.py
    update port

--- docker/devops_engineer/docker/part-2/consul/app.py
+++ docker/devops_engineer/docker/part-2/consul/app.py
@@ -80,5 +80,5 @@ def index():
     return render_template("index.html", result_message=result_message)
 
 if __name__ == "__main__":
-    # Run Flask on port 5000, accessible on all interfaces
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Run Flask on port 5010, accessible on all interfaces
+    app.run(host="0.0.0.0", port=5010, debug=True)
