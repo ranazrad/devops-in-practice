d574bd5883ee2db068ce50dbb6014ded7991838d
Ran Azrad <ranazrad@gmail.com>
2025-02-16 08:08:59 +0200

    Update app.py

--- docker/devops_engineer/docker-compose/part-1/lab-02/app/app.py
+++ docker/devops_engineer/docker-compose/part-1/lab-02/app/app.py
@@ -10,4 +10,4 @@ def home():
     return f"Hello, Docker! This page has been visited {visits} times."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5010)
